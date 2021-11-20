127705d8f2b4cdb5ef1ce8f77514528738071f98
EdMV <edward.munoz@buyersedgeplatform.com>
2021-11-20 11:48:40 -0500

    Add sample_request to API

--- mineco.py
+++ mineco.py
@@ -3,29 +3,52 @@ import requests
 
 class MinecoAPIMensual:
     BASE_URL = 'https://apps5.mineco.gob.pe/transparencia/mensual/Navegar_6.aspx'
-    parameters = {
-        '_tgt': 'xls',  # File format
-        '_uhc': 'yes',  # ?
-        '0': '',  # ?
-        '1': 'E',  # Nivel de Gobierno
-        '30': '0082',  # Categoría Presupuestal
-        '21': '02',  # Departamento
-        '23': '1',  # Mes
-        '31': '',  # ?
-        'y': '2021',  # Año
-        'ap': 'Proyecto',  # ?
-        'cpage': '1',  # Página?
-        'psize': '400'  # Records per page?
-    }
 
-    def base_export(self):
-        return requests.get(self.BASE_URL, params=self.parameters)
+    @staticmethod
+    def parameters(
+        cod_nivel_gobierno,
+        cod_categoria_presupuestal,
+        cod_departamento,
+        mes,
+        año,
+    ):
+        return {
+            '_tgt': 'xls',  # File format
+            '_uhc': 'yes',  # ?
+            '0': '',  # ?
+            '1': cod_nivel_gobierno,  # Nivel de Gobierno
+            '30': cod_categoria_presupuestal,  # Categoría Presupuestal
+            '21': cod_departamento,  # Departamento
+            '23': mes,  # Mes
+            '31': '',  # ?
+            'y': año,  # Año
+            'ap': 'Proyecto',  # ?
+            'cpage': '1',  # Página?
+            'psize': '400'  # Records per page?
+        }
 
+    def sample_request(self):
+        params = self.parameters(
+            cod_nivel_gobierno='E',
+            cod_categoria_presupuestal='0083',
+            cod_departamento=None,
+            mes=None,
+            año=2021
+        )
+        response = requests.get(
+            self.BASE_URL, params=params)
+        return response
+
+    @staticmethod
+    def save_response(response, filename):
+        with open(filename, 'wb') as f:
+            f.write(response.content)
 
 def main():
     mineco_api_mensual = MinecoAPIMensual()
-    response = mineco_api_mensual.base_export()
-    print(response)
+    response = mineco_api_mensual.sample_request()
+    mineco_api_mensual.save_response(response, 'test_export.xls')
+    print(response.url)
 
 
 if __name__ == "__main__":
