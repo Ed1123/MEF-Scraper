49ab9b55312f9b1413d8b3359628a6b6b529c9d7
EdMV <edward.munoz@buyersedgeplatform.com>
2021-11-21 18:44:54 -0500

    Add get_url func and basic spider

--- aliaxis/spiders/mef_1.py
+++ aliaxis/spiders/mef_1.py
@@ -1,10 +1,35 @@
+from urllib import parse
+
 import scrapy
+
+
+def get_url(cod_nivel_gobierno='E', cod_categoria_presupuestal='0083', cod_departamento=None, mes=None, año=2021):
+    url = 'https://apps5.mineco.gob.pe/transparencia/mensual/Navegar_6.aspx'
+    params = {
+        '_tgt': 'xls',  # File format
+        '_uhc': 'yes',  # ?
+        '0': '',  # ?
+        '1': cod_nivel_gobierno,  # Nivel de Gobierno
+        '30': cod_categoria_presupuestal,  # Categoría Presupuestal
+        '21': cod_departamento,  # Departamento
+        '23': mes,  # Mes
+        '31': '',  # ?
+        'y': año,  # Año
+        'ap': 'Proyecto',  # ?
+        'cpage': '1',  # Página
+        'psize': '1000000'  # Records per page
+    }
+    params = {k: v for k, v in params.items() if v is not None}
+    params = parse.urlencode(params)
+    return f'{url}?{params}'
 
 
 class Mef1Spider(scrapy.Spider):
     name = 'mef_1'
-    allowed_domains = ['https://apps5.mineco.gob.pe/transparencia/mensual']
-    start_urls = ['http://https://apps5.mineco.gob.pe/transparencia/mensual/']
+    start_urls = [get_url()]
 
     def parse(self, response):
-        pass
+        rows = response.xpath('//table[3]//tr')
+        for row in rows:
+            data = row.xpath('./td/text()').getall()
+            yield {'data': [d.strip() for d in data]}
