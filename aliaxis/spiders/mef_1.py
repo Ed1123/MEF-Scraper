9d6716bdfc6df35766f45149008759c7eea16ede
EdMV <edward.munoz@buyersedgeplatform.com>
2021-11-23 22:28:58 -0500

    Add Url class

--- aliaxis/spiders/mef_1.py
+++ aliaxis/spiders/mef_1.py
@@ -1,36 +1,16 @@
 # %%
 import itertools
+from dataclasses import dataclass
 from urllib import parse
 
 import scrapy
 
 
 class MinecoAPI:
-    base_url = 'https://apps5.mineco.gob.pe/transparencia/mensual/Navegar_6.aspx'
-
     cod_niveles_gobierno = ['E', 'M', 'R']
     cod_categorias_presupuestales = ['0083']
     cod_departamentos = range(1, 25)
     meses = range(1, 13)
-
-    def _get_url(self, cod_nivel_gobierno='E', cod_cat_presupuestal='0083', cod_departamento=None, mes=None, año=2021):
-        params = {
-            '_tgt': 'xls',  # File format
-            '_uhc': 'yes',  # ?
-            '0': '',  # ?
-            '1': cod_nivel_gobierno,  # Nivel de Gobierno
-            '30': cod_cat_presupuestal,  # Categoría Presupuestal
-            '21': cod_departamento,  # Departamento
-            '23': mes,  # Mes
-            '31': '',  # ?
-            'y': año,  # Año
-            'ap': 'Proyecto',  # ?
-            'cpage': '1',  # Página
-            'psize': '1000000'  # Records per page
-        }
-        params = {k: v for k, v in params.items() if v is not None}
-        params = parse.urlencode(params)
-        return f'{self.base_url}?{params}'
 
     def get_pia_pim_urls(self):
         parameters = itertools.product(
@@ -38,7 +18,7 @@ class MinecoAPI:
             self.cod_categorias_presupuestales
         )
 
-        return [self._get_url(cod_nivel_gobierno=i, cod_cat_presupuestal=j) for i, j in parameters]
+        return [Url(cod_nivel_gobierno=i, cod_cat_presupuestal=j) for i, j in parameters]
 
     def get_monthly_report_urls(self):
         parameters = itertools.product(
@@ -48,7 +28,40 @@ class MinecoAPI:
             self.meses
         )
 
-        return [self._get_url(*args) for args in parameters]
+        return [Url(*args) for args in parameters]
+
+
+class Url:
+    base_url = 'https://apps5.mineco.gob.pe/transparencia/mensual/Navegar_6.aspx'
+
+    def __init__(self, cod_nivel_gobierno='E', cod_cat_presupuestal='0083', cod_departamento=None, mes=None, año=2021):
+        self.cod_nivel_gobierno = cod_nivel_gobierno
+        self.cod_cat_presupuestal = cod_cat_presupuestal
+        self.cod_departamento = cod_departamento
+        self.mes = mes
+        self.año = año
+
+    def _get_params_dict(self):
+        params = {
+            '_tgt': 'xls',  # File format
+            '_uhc': 'yes',  # ?
+            '0': '',  # ?
+            '1': self.cod_nivel_gobierno,  # Nivel de Gobierno
+            '30': self.cod_cat_presupuestal,  # Categoría Presupuestal
+            '21': self.cod_departamento,  # Departamento
+            '23': self.mes,  # Mes
+            '31': '',  # ?
+            'y': self.año,  # Año
+            'ap': 'Proyecto',  # ?
+            'cpage': '1',  # Página
+            'psize': '1000000'  # Records per page
+        }
+        params = {k: v for k, v in params.items() if v is not None}
+        params = parse.urlencode(params)
+        return params
+
+    def get(self):
+        return f'{self.base_url}?{self._get_params_dict()}'
 
 
 # %%
@@ -60,7 +73,7 @@ class Mef1Spider(scrapy.Spider):
         mineco_api = MinecoAPI()
         urls = mineco_api.get_monthly_report_urls() + mineco_api.get_pia_pim_urls()
         for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
+            yield scrapy.Request(url=url.get())
 
     def parse(self, response):
         row_headers = [
