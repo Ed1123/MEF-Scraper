68d45dd0d00be8bcc0f16d2a991d18f18290aefd
EdMV <edward.munoz@buyersedgeplatform.com>
2021-11-26 22:57:57 -0500

    Upload data to S3 and local folder

--- aliaxis/settings.py
+++ aliaxis/settings.py
@@ -7,6 +7,12 @@
 #     https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
 #     https://docs.scrapy.org/en/latest/topics/spider-middleware.html
 
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
 BOT_NAME = 'aliaxis'
 
 SPIDER_MODULES = ['aliaxis.spiders']
@@ -37,34 +43,34 @@ ROBOTSTXT_OBEY = False
 #TELNETCONSOLE_ENABLED = False
 
 # Override the default request headers:
-#DEFAULT_REQUEST_HEADERS = {
+# DEFAULT_REQUEST_HEADERS = {
 #   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
 #   'Accept-Language': 'en',
-#}
+# }
 
 # Enable or disable spider middlewares
 # See https://docs.scrapy.org/en/latest/topics/spider-middleware.html
-#SPIDER_MIDDLEWARES = {
+# SPIDER_MIDDLEWARES = {
 #    'aliaxis.middlewares.AliaxisSpiderMiddleware': 543,
-#}
+# }
 
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
+# DOWNLOADER_MIDDLEWARES = {
 #    'aliaxis.middlewares.AliaxisDownloaderMiddleware': 543,
-#}
+# }
 
 # Enable or disable extensions
 # See https://docs.scrapy.org/en/latest/topics/extensions.html
-#EXTENSIONS = {
+# EXTENSIONS = {
 #    'scrapy.extensions.telnet.TelnetConsole': None,
-#}
+# }
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
+# ITEM_PIPELINES = {
 #    'aliaxis.pipelines.AliaxisPipeline': 300,
-#}
+# }
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
@@ -86,3 +92,16 @@ ROBOTSTXT_OBEY = False
 #HTTPCACHE_DIR = 'httpcache'
 #HTTPCACHE_IGNORE_HTTP_CODES = []
 #HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+
+# AWS S3
+aws_key = os.getenv('AWS_KEY')
+aws_secret = os.getenv('AWS_SECRET')
+aws_bucket = os.getenv('AWS_BUCKET')
+FEEDS = {
+    f's3://{aws_key}:{aws_secret}@{aws_bucket}/mef_1_output/%(time)s.csv': {
+        'format': 'csv',
+    },
+    'mef_1_output/%(time)s.csv': {
+        'format': 'csv',
+    }
+}
