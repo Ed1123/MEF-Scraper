525815462fd36391ebfe0220479872d3ceb90094
Ed1123 <44955050+Ed1123@users.noreply.github.com>
2022-01-12 18:48:09 -0500

    Fix bug with year 2022

--- aliaxis/spiders/mef_2.py
+++ aliaxis/spiders/mef_2.py
@@ -4,16 +4,25 @@ import scrapy
 class Mef2Spider(scrapy.Spider):
     name = 'mef_2'
     allowed_domains = [
-        'https://apps5.mineco.gob.pe/bingos/seguimiento_pi/Navegador/default.aspx']
+        'https://apps5.mineco.gob.pe/bingos/seguimiento_pi/Navegador/default.aspx'
+    ]
     start_urls = [
         'https://apps5.mineco.gob.pe/bingos/seguimiento_pi/Navegador/Navegar_2.aspx?'
-        '_tgt=xls&_uhc=yes&0=&31=&y=2021&cpage=1&psize=1000000'
+        '_tgt=xls&_uhc=yes&0=&33=&y=2022&cpage=1&psize=1000000'
     ]
 
     def parse(self, response):
         row_headers = [
-            'Proyecto', 'Costo', 'Ejecución al año 2019', 'Ejecución año 2020', '2021 PIA',
-            '2021 PIM', '2021 Devengado', '2021 Avance %', 'Ejecución Total', 'Avan % Total'
+            'Proyecto',
+            'Costo',
+            'Ejecución al año 2019',
+            'Ejecución año 2020',
+            '2021 PIA',
+            '2021 PIM',
+            '2021 Devengado',
+            '2021 Avance %',
+            'Ejecución Total',
+            'Avan % Total',
         ]
         for row in response.xpath('//table[3]//tr'):
             data = row.xpath('./td/text()').getall()
