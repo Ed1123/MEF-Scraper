da0c781dae3495b0a3a31803a29a36c03103e91d
Ed1123 <44955050+Ed1123@users.noreply.github.com>
2022-01-19 15:38:34 -0500

    Fixing headers years

--- aliaxis/spiders/mef_2.py
+++ aliaxis/spiders/mef_2.py
@@ -3,30 +3,29 @@ import scrapy
 
 class Mef2Spider(scrapy.Spider):
     name = 'mef_2'
-    allowed_domains = [
-        'https://apps5.mineco.gob.pe/bingos/seguimiento_pi/Navegador/default.aspx'
-    ]
     start_urls = [
         'https://apps5.mineco.gob.pe/bingos/seguimiento_pi/Navegador/Navegar_2.aspx?'
         '_tgt=xls&_uhc=yes&0=&33=&y=2022&cpage=1&psize=1000000'
     ]
 
     def parse(self, response):
-        row_headers = [
+        i = self.start_urls[0].find('&y=')
+        year = int(self.start_urls[0][i + 3 : i + 7])
+        headers = [
             'Proyecto',
             'Costo',
-            'Ejecución al año 2019',
-            'Ejecución año 2020',
-            '2021 PIA',
-            '2021 PIM',
-            '2021 Devengado',
-            '2021 Avance %',
+            f'Ejecución al año {year - 2}',
+            f'Ejecución año {year - 1}',
+            f'{year} PIA',
+            f'{year} PIM',
+            f'{year} Devengado',
+            f'{year} Avance %',
             'Ejecución Total',
             'Avan % Total',
         ]
         for row in response.xpath('//table[3]//tr'):
             data = row.xpath('./td/text()').getall()
             data = [d.strip() for d in data]
-            item = {tittle: value for tittle, value in zip(row_headers, data)}
+            item = {tittle: value for tittle, value in zip(headers, data)}
             item['CUI'] = item['Proyecto'][:7]
             yield item
